677805aeafed65218959743a477e3c175472f025
Manik Dingra <manikdhingra0582@gmail.com>
2022-10-08 13:17:23 +0530

    React router used and categories working implemented

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,4 +1,9 @@
 import "./App.css";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes
+} from "react-router-dom";
 
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
@@ -8,8 +13,31 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<Navbar />
-				<News pageSize={10} country="in" category='sports' />
+				<Router>
+
+					<Navbar />
+					<Routes>
+						<Route exact path="/"
+							element={<News key="general" pageSize={10} country="in" category='general' />} />
+						<Route exact path="/Business"
+							element={<News key="business" pageSize={10} country="in" category='business' />} />
+						<Route exact path="/Sports"
+							element={<News key="sports" pageSize={10} country="in" category='sports' />} />
+						<Route exact path="/Technology"
+							element={<News key="technology" pageSize={10} country="in" category='technology' />} />
+						<Route exact path="/Health"
+							element={<News key="health" pageSize={10} country="in" category='health' />} />
+						<Route exact path="/Science"
+							element={<News key="science" pageSize={10} country="in" category='science' />}
+						/>
+						<Route exact path="/Entertainment"
+							element={<News key="entertainment" pageSize={10} country="in" category='entertainment' />}
+						/>
+						<Route exact path="/General"
+							element={<News key="general" pageSize={10} country="in" category='general' />}
+						/>
+					</Routes>
+				</Router>
 			</div>
 		);
 	}
